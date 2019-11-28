04a2f6b229abf8204af828ac0a820d9322df50dc
kikilagou <kiki-1@hotmail.co.uk>
2019-11-28 23:03:33 +0000

    classifying poker hands using the UCI poker hands dataset

---
+++ poker_hands/model.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# S = suite, C = card
+headings = ['S1', 'C1', 'S2', 'C2', 'S3', 'C3', 'S4', 'C4', 'S5', 'C5', 'hand']
+
+# import data
+train = pd.read_csv("../../../Documents/Data/poker-hand-training-true.txt", names=headings)
+test = pd.read_csv("../../../Documents/Data/poker-hand-testing.txt", names=headings)
+
+print(train.head())
+print(train.shape)
+print(test.shape)
+
