23f14d15b3843da7cc8e11e4ff06f8155432c1a3
kikilagou <kiki-1@hotmail.co.uk>
2019-12-28 22:37:59 +0000

    finished knn with accuracy of around 80%

--- poker_hands/knn.py
+++ poker_hands/knn.py
@@ -1,12 +1,11 @@
 import pandas as pd
 from copy import deepcopy
 from sklearn.model_selection import train_test_split
-import numpy as np
 
 NAMES = ['id', 'RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe', 'type']
 FEATURES = ['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe']
 TRAIN_TEST_RATIO = 0.8
-k = 5
+k = 3
 
 def load_and_prep_data(csv):
     """
@@ -17,10 +16,10 @@ def load_and_prep_data(csv):
     data = pd.read_csv(csv, names=NAMES)
     data = data.drop('id', axis=1)
 
-    X = data.drop('type', axis=1)
+    # X = data.drop('type', axis=1)
     y = data['type']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1-TRAIN_TEST_RATIO, random_state=4)
+    X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=1-TRAIN_TEST_RATIO, random_state=4)
 
     return data, X_train, X_test, y_train, y_test
 
@@ -31,6 +30,7 @@ def calc_euclidean_distance(X_train, unseen_point):
     Get the neighbours
     :return:
     """
+    print(unseen_point)
     arr = []
     X_train_dist = deepcopy(X_train)
     for index, row in X_train.iterrows():
@@ -46,12 +46,32 @@ def calc_euclidean_distance(X_train, unseen_point):
 
     return neighbours
 
+def predict_class(neighbours):
+    mode = neighbours.loc[:, "type"].mode()
+
+    return mode
 
 
-df, X_train, X_test, y_train, y_test = load_and_prep_data("glass.data")
+if __name__ == '__main__':
+    # load the data
+    df, X_train, X_test, y_train, y_test = load_and_prep_data("glass.data")
 
-print(df.head())
-# dataset_minmax(df)
-# print(X_test.iloc[[0]])
-print(calc_euclidean_distance(X_train, X_test.iloc[[0]]))
-#
+    print(X_train.head())
+    print(X_test.head())
+    print(y_test.head())
+    print(y_train.head())
+
+    correct = 0
+
+    print()
+
+    for i in range(len(X_test)):
+        row = X_test.iloc[[i]]
+        neighbours = calc_euclidean_distance(X_train, row)
+        classification_prediction = predict_class(neighbours).iloc[0]
+        actual_classification = row['type'].iloc[0]
+        if actual_classification == classification_prediction:
+            correct += 1
+
+    accuracy = correct / len(X_test.index)
+    print("Accuracy: {}".format(accuracy))
